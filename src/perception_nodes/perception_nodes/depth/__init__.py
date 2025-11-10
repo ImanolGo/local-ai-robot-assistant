b85ce3f9c09574070630e33e2b2f022d798b4335
Imanol Gomez <imanol@steinergomez.com>
2025-11-10 18:54:09 +0100

    Deleted rt_monodepth

--- src/perception_nodes/perception_nodes/depth/__init__.py
+++
@@ -1,4 +0,0 @@
-from .rt_monodepth_inference import RTMonoDepthInference
-from .rt_monodepth_preprocessing import RTMonoDepthPreprocessor
-
-__all__ = ["RTMonoDepthInference", "RTMonoDepthPreprocessor"]
--- src/perception_nodes/perception_nodes/depth/networks/__init__.py
+++
@@ -1,4 +0,0 @@
-from .depth_decoder import DepthDecoder  # noqa E401
-from .pose_cnn import PoseCNN  # noqa E401
-from .pose_decoder import PoseDecoder  # noqa E401
-from .resnet_encoder import ResnetEncoder  # noqa E401
